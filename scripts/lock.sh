85c15dc58df3f8212daada0c2aefd982146ac2df
Doshirae <doshirae@lilo.org>
2017-12-02 23:37:37 +0100

    Changed date format of lockscreen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -13,7 +13,6 @@ scrot /tmp/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 
-BACKGROUND=`xrdb -query | grep "color8:" | cut -d : -f 2 | cut -c 3-`"FF"
 BLUE=`xrdb -query | grep "color4:" | cut -d : -f 2 | cut -c 3-`"FF"
 RED=`xrdb -query | grep "color1:" | cut -d : -f 2 | cut -c 3-`"FF"
 RED_TRANSPARENT=`xrdb -query | grep "color1:" | cut -d : -f 2 | cut -c 3-`"30"
@@ -23,15 +22,15 @@ CYAN=`xrdb -query | grep "color6:" | cut -d : -f 2 | cut -c 3-`"FF"
 GREY=`xrdb -query | grep "color7:" | cut -d : -f 2 | cut -c 3-`"FF"
 
 i3lock -n -k -e -i $TMPBG \
-	--indicator \
-	--keyhlcolor=$BLUE --bshlcolor=$PURPLE --separatorcolor=$BACKGROUND \
-	--insidecolor=00000000 --ringcolor=$BACKGROUND \
+	--keyhlcolor=$BLUE --bshlcolor=$PURPLE --separatorcolor=$BLUE \
+	--insidecolor=00000000 --ringcolor=00000000 \
 	--insidevercolor=00000000 --ringvercolor=$GREEN \
 	--insidewrongcolor=$RED_TRANSPARENT --ringwrongcolor=$RED \
 	--line-uses-inside \
 	--indpos="x+w/2:y+h/2" \
 	--radius=250 --veriftext="" --wrongtext="" --ring-width=10 \
-	--timecolor=$GREY --datecolor=$GREY \
+	--timecolor=$GREY \
+	--datecolor=$GREY --datestr="%A %e %b %Y"\
 	--timepos="ix-cw/2:iy-200-ch/2"
 
 revert
