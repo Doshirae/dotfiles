6f15b245a02e9288c014aea3a95cb0da3b0c7ee5
Doshirae <doshirae@lilo.org>
2017-12-02 19:21:52 +0100

    Made a better lock screen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env sh
 
 revert() {
-    xset dpms 0 0 0
+	xset dpms 0 0 0
 }
 
 trap revert HUP INT TERM
-xset +dpms dpms 10 10 10
+xset +dpms dpms 30 30 30
 
 ICON=$HOME/.dotfiles/i3/birb-lock.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -u -i $TMPBG
+
+BACKGROUND=`xrdb -query | grep "background:" | cut -d : -f 2 | cut -c 3-`"FF"
+BLUE=`xrdb -query | grep "color4:" | cut -d : -f 2 | cut -c 3-`"FF"
+RED=`xrdb -query | grep "color1:" | cut -d : -f 2 | cut -c 3-`"FF"
+RED_TRANSPARENT=`xrdb -query | grep "color1:" | cut -d : -f 2 | cut -c 3-`"30"
+GREEN=`xrdb -query | grep "color2:" | cut -d : -f 2 | cut -c 3-`"FF"
+PURPLE=`xrdb -query | grep "color5:" | cut -d : -f 2 | cut -c 3-`"FF"
+CYAN=`xrdb -query | grep "color6:" | cut -d : -f 2 | cut -c 3-`"FF"
+GREY=`xrdb -query | grep "color7:" | cut -d : -f 2 | cut -c 3-`"FF"
+
+
+HEIGHT=`xdpyinfo | awk '/dimensions:/ { print $2; exit }' | cut -d x -f 2`
+RADIUS=`echo "$HEIGHT/2" | bc`
+
+i3lock -n -k -e -i $TMPBG \
+	--keyhlcolor=$BLUE --bshlcolor=$PURPLE --separatorcolor=$BACKGROUND \
+	--insidecolor=00000000 --ringcolor=$BACKGROUND \
+	--insidevercolor=00000000 --ringvercolor=$GREEN \
+	--insidewrongcolor=$RED_TRANSPARENT --ringwrongcolor=$RED \
+	--line-uses-inside \
+	--indpos="x+w/2:y+h/2" \
+	--radius=$RADIUS --veriftext="" --wrongtext="" --ring-width=10 \
+	--timecolor=$GREY --datecolor=$GREY \
+	--timepos="ix-cw/2:iy-200-ch/2"
 
 revert
