996b408c0c27c98ada0ec753d9106c99fcbd178c
Doshirae <doshirae@doshirae.fr>
2018-01-14 16:11:56 +0100

    Merged all ruby scripts into 1 big loop + deleted old logout script

---
+++ scripts/master_script.rb
@@ -0,0 +1,100 @@
+# battery_notify ==>
+$state = `acpi -b | awk '{ print $3 }'`[0..-3]
+def battery_notify
+	oldstate = $state
+	newstate, percentage = %x(acpi | awk '{ print $3 $4 }')[0..-3].split ","
+	percentage = percentage.to_i
+
+	if newstate != oldstate
+		if percentage >= 98
+			`notify-send -u low "Batterie pleine"`
+		else
+			`notify-send -u low "#{newstate}"`
+		end
+	end
+
+	if newstate == "Discharging"
+		if percentage <= 10
+			# `xdotool key ctrl+space`
+			`notify-send -u critical "Branche ton PC. Vite." "#{percentage}%"`
+		elsif percentage <= 20
+			# `xdotool key ctrl+space`
+			`notify-send -u normal "Batterie faible" "#{percentage}%"`
+		end
+	end
+
+	$state = newstate
+end
+# <==
+
+# wallpaper ==>
+$changed = false
+$daytime = "rien"
+def wallpaper
+	hour = Time.now.hour
+
+	matin = (5..11)
+	midi = (12..15)
+	aprem = (16..21)
+	nuit = [22, 23, 0, 1, 2, 3, 4]
+
+	if matin.include? hour and $daytime != "matin"
+		$daytime = "matin"
+		$changed = true
+	elsif midi.include? hour and $daytime != "midi"
+		$daytime = "midi"
+		$changed = true
+	elsif aprem.include? hour and $daytime != "aprem"
+		$daytime = "aprem"
+		$changed = true
+	elsif nuit.include? hour and $daytime != "nuit"
+		$daytime = "nuit"
+		$changed = true
+	end
+
+	if $changed
+		files = %x(ls $HOME/.dotfiles/i3/backgrounds/*-#{$daytime}*).split "\n"
+		bg = files.shuffle.first
+		`feh --bg-scale #{bg}`
+
+		$changed = false
+	end
+end
+# <==
+
+# kanban_todo ==>
+def kanban_todo
+	out = `awk -F , '{ print $1 ", " $2 ", " $3 }' ~/.kanban.csv | sed 's/"//g'` # recup le tag + truc à faire
+	out = out.split("\n")                                         # Formatage
+	out.shift                                                     # On se fiche de la première ligne, c'est le titre des colonnes
+
+	out = out.map { |line| line.split(",") }                      # On sépare les lignes pour avoir un tableau à 2 dimensions
+		.reject { |line| line[1] == "T" }                           # On dégage une ligne inutile
+		.select { |line| line[0] == "TODO" }                        # On garde que ce qui est TODO
+	out.map(&:shift)                                              # On dégage les titres, dtf c'est forcément TODO
+	out = out.group_by { |line| line[0] }                         # On regroupe par tag
+
+	File.open("/home/doshirae/.todo.md", 'w') do |file|
+		out.each_key do |key|
+			file.write "#{key.strip.capitalize} :\n"                  # On écrit le tag avec la 1ère lettre en majuscule
+			things = out[key].flatten.reject { |item| item == key }   # On récup les items correspondant au tag
+			things.each do |thing|
+				file.write "   -#{thing}\n"                             # On écrit les tags un a un
+			end
+		end
+	end
+  %x(notify-send "Trucs à faire" "$(cat ~/.todo.md)")
+end
+# <==
+
+seconds = 0
+loop do
+	if seconds % 600 == 0  # toutes les 10 minutes
+		kanban_todo()
+		seconds = 0
+	end
+	wallpaper()
+	battery_notify()
+	sleep(5)
+	seconds += 5
+end
--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 ls /home/doshirae/Pictures/Wallpapers/Choices | sort -R |tail -1 | while read file; do
 	feh --bg-scale /home/doshirae/Pictures/Wallpapers/Choices/"$file"
 done
